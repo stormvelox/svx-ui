3bbb63eebef020a40e40c707d78dd6e22c963903
NohbdyAhtall <ryanmcgillisaparadox@gmail.com>
2025-04-04 20:35:39 -0700

    lightworks

--- control-ui/start_menu.gd
+++ control-ui/start_menu.gd
@@ -1,63 +1,14 @@
-#region header
-## Description here (if using class_name) also see:[br]
-## https://docs.godotengine.org/en/latest/tutorials/scripting/gdscript/gdscript_documentation_comments.html?highlight=bbcode#bbcode-and-class-reference
+## Start Menu
+##
+## A menu... for starting.
+class_name StartMenu extends Control
+## Test
 
-#@tool
-#class_name Unknown
-extends Control
-## # docstring
-# doscstring test
-#endregion
-#region variables
-#region signals
-#endregion
-#region enums
-#enum InlineEnum {ZERO, ONE, TWO}
-#enum SomeEnum {
-#    ZERO,
-#    ONE,
-#    TWO,
-#}
-#endregion
-#region constants
-#const test : float =  0.00
-#endregion
-#region exported variables
-#@export var show_debug : bool = false
-#endregion
-#region public variables
-#var public_var : bool = true
-#endregion
-#region private variables
-#var _private_var : bool = false
-#endregion
-#region onready variables
-#@onready var onready_var : bool = true
-#endregion
-#endregion variables
-#region methods
-#region optional built-in virtual _init method
-#func _init() -> void:
-#    pass
-#endregion
-
-#region built-in virtual _ready method
+## At-ready
 func _ready() -> void:
+	## test2
 	pass
-#endregion
 
-#region remaining built-in virtual methods
+## Per-Process
 func _process(delta: float) -> void:
 	pass
-
-#endregion
-#region public methods
-#func public_method() -> void:
-#    pass
-#endregion
-
-#region private methods
-#func _on_some_signal() -> void:
-#    pass
-#endregion
-#endregion methods
---
+++ light_testing_scene.gd
@@ -0,0 +1,34 @@
+#region header
+## Description
+#@tool
+class_name LightTestingScene
+extends Node3D
+## # docstring
+# doscstring description
+#endregion
+#region variables
+#signal
+#enum
+#const
+#@export
+#@export
+#var
+#var _
+#@onready
+#endregion
+#region methods
+#func _
+func _init() -> void:
+	light_groups = get_groups()
+	light_groups.has()
+
+func _ready() -> void:
+	var all_lights_possible : Dictionary
+	# get all lights\
+
+	lights_detected
+	for (i in )
+func _process(delta: float) -> void:
+	pass
+#func
+#endregion
