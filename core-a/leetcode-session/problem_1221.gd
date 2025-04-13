0989912cc2a05652ba1180b23a2c78d262674086
NohbdyAhtall <ryanmcgillisaparadox@gmail.com>
2025-04-13 16:26:06 -0700

    Leetcode challenge progress hah

--- core-a/leetcode-session/problem_1221.gd
+++ core-a/leetcode-session/problem_1221.gd
@@ -6,13 +6,16 @@ extends Node
 #const
 #@export
 #region examples
-var example1 : String = "RLRRLLRLRL" # Explanation: s can be split into "RL",
+var example1 : String = "RLRRLLRLRL" 
+# Explanation: s can be split into "RL",
 #  "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
-var example2 : String = "RLRRRLLRLL" # s can be split into "RL", "RRRLLRLL",
+var example2 : String = "RLRRRLLRLL"
+# s can be split into "RL", "RRRLLRLL",
 #  each substring contains same number of 'L' and 'R'. Note that s cannot be
 #  split into "RL", "RR", "RL", "LR", "LL", because the 2nd and 5th substrings
 #  are not balanced.
-var example3 : String = "LLLLRRRR" # Explanation: s can be split into "LLLLRRRR".
+var example3 : String = "LLLLRRRR" 
+# Explanation: s can be split into "LLLLRRRR".
 #endregion
 var output : int
 #var _
@@ -44,23 +47,31 @@ func ensure_constraints(s: String) -> bool:
 func is_correct_length(s: String) -> bool:
 	## 2 <= s.length <= 1000
 	var len = s.length()
-	if (len <= 2):
+	
+	if (len >= 2 && len <= 1000):
+		print("Length is correct.")
 		return true
 	else:
+		print("Length is incorrect.")
 		return false
 
 func is_l_or_r(s: String) -> bool:
 	## s is either 'L' or 'R'
-	if(s):
+	if(s.to_upper() == 'L' || 'R'):
+		print("String '", s, "' is 'L' or 'R'.")
 		return true
 	else:
 		return false
 
 func is_balanced_string(s: String) -> bool:
 	## s is a balanced string
-	if(true):
+	var l_count : int = s.count('L')
+	var r_count : int = s.count('R')
+	if(l_count == r_count):
+		print("R and L counts are equal.")
 		return true
 	else:
+		print("R and L counts are NOT equal.")
 		return false
 
 func _process(delta: float) -> void:
